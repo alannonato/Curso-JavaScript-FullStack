65fb3c38c47e9ae11443c2319612a5d0e6b26705
Alan da Paixao Nonato Santos <alan.paixao@totvs.com.br>
2020-05-31 19:09:17 -0300

    Nova base de exercicios feitos 28/05/2020

--- exercicios-js/fundamentos/nullUndefined.js
+++ exercicios-js/fundamentos/nullUndefined.js
@@ -1,4 +1,22 @@
 let valor // não icializada 
 console.log(valor)
 
-valor = null // ausencia de valor
+valor = null // Ausencia de valor 
+console.log(valor)
+    //console.log(valor.toString())//ERRO
+
+const produto = {}
+console.log(produto.preco)
+console.log(produto)
+
+produto.preco = 3.50
+console.log(produto.toFixed(2))
+
+produto.preco = undefined // evite atribuir undefined
+console.log(!!produto.preco)
+    //delete produto.preco
+console.log(produto)
+
+produto.preco = null // sem preço 
+console.log(!!produto.preco)
+console.log(produto)
